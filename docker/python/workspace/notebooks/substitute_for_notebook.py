c82fa889cfb6da640394dd34f1f12ef74fb2f3bf
Seiya Sasaki <sirukufarios@gmail.com>
2022-10-11 18:31:57 +0900

    integrate dataset arrangement processing (substitute_for_notebook.py)

--- docker/python/workspace/notebooks/substitute_for_notebook.py
+++ docker/python/workspace/notebooks/substitute_for_notebook.py
@@ -7,7 +7,11 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 
+# For Sample (VOC2008 dataset)
 PARENT_DIR: str = "../VOCdevkit/VOC2008"
+TRAIN_FILENAME_LIST_FILE: str = "train_train.txt"
+VALIDATION_FILENAME_LIST_FILE: str = "train_val.txt"
+MV_TARGET_PARENT_DIR: str = "../tr_data"
 
 
 def extract_filenames(target_file_name: str, ext: str = '.jpg') -> List[str]:
@@ -34,20 +38,20 @@ def copy_files(fname_list: List[str], origin_dir: str, target_dir: str):
 ##################################################################
 # Move image files
 ##################################################################
-train_filenames: List[str] = extract_filenames(target_file_name="train_train.txt")
-val_filenames: List[str] = extract_filenames(target_file_name="train_val.txt")
+train_filenames: List[str] = extract_filenames(target_file_name=TRAIN_FILENAME_LIST_FILE)
+val_filenames: List[str] = extract_filenames(target_file_name=VALIDATION_FILENAME_LIST_FILE)
 
 origin_dir: str = os.path.join(PARENT_DIR, "JPEGImages")
-copy_files(train_filenames, origin_dir, target_dir="../tr_data/images/train")
-copy_files(val_filenames, origin_dir, target_dir="../tr_data/images/val")
+copy_files(train_filenames, origin_dir, target_dir=MV_TARGET_PARENT_DIR + "/images/train")
+copy_files(val_filenames, origin_dir, target_dir=MV_TARGET_PARENT_DIR + "/images/val")
 
 
 ##################################################################
 # Move annotation files
 ##################################################################
-train_annotation_filenames: List[str] = extract_filenames(target_file_name="train_train.txt", ext='.txt')
-val_annotation_filenames: List[str] = extract_filenames(target_file_name="train_val.txt", ext='.txt')
+train_annotation_filenames: List[str] = extract_filenames(target_file_name=TRAIN_FILENAME_LIST_FILE, ext='.txt')
+val_annotation_filenames: List[str] = extract_filenames(target_file_name=VALIDATION_FILENAME_LIST_FILE, ext='.txt')
 
 origin_dir: str = os.path.join("../tr_data", "format4yolo")
-copy_files(train_annotation_filenames, origin_dir, target_dir="../tr_data/labels/train")
-copy_files(val_annotation_filenames, origin_dir, target_dir="../tr_data/labels/val")
+copy_files(train_annotation_filenames, origin_dir, target_dir=MV_TARGET_PARENT_DIR + "/labels/train")
+copy_files(val_annotation_filenames, origin_dir, target_dir=MV_TARGET_PARENT_DIR + "/labels/val")
